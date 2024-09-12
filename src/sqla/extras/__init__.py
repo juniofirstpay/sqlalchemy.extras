2fd908adda20d38187b98e8ab43dc848e4cfef6a
Nirbhay Gupta <nirbhay.gupta@junio.in>
2024-09-12 17:22:22 +0530

    [fix] session wrapper fixes

--- src/sqla/extras/__init__.py
+++ src/sqla/extras/__init__.py
@@ -3,8 +3,13 @@ import functools
 from contextvars import ContextVar
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
-async_session_context: ContextVar[AsyncSession] = ContextVar("async_session_context", default=None)
-async_sessionmaker_context: ContextVar[async_sessionmaker] = ContextVar("async_sessionmaker_context", default=None)
+async_session_context: ContextVar[AsyncSession] = ContextVar(
+    "async_session_context", default=None
+)
+async_sessionmaker_context: ContextVar[async_sessionmaker] = ContextVar(
+    "async_sessionmaker_context", default=None
+)
+
 
 def _session_wrap(session_factory: async_sessionmaker, async_callable, is_nested=False):
     @functools.wraps(async_callable)
@@ -13,7 +18,7 @@ def _session_wrap(session_factory: async_sessionmaker, async_callable, is_nested
 
         if is_nested == True and existing_session is None:
             raise Exception("Parent session not started")
-        
+
         if is_nested == True and existing_session:
             async with existing_session.begin_nested() as nested:
                 try:
@@ -23,22 +28,22 @@ def _session_wrap(session_factory: async_sessionmaker, async_callable, is_nested
                 except Exception as e:
                     nested.rollback()
 
-
         if existing_session:
             async with existing_session:
                 return await async_callable(*args, **kwargs)
-                
+
         async with session_factory() as session:
             async with session:
                 async_session_context.set(session)
                 return await async_callable(*args, **kwargs)
-    
+
     return session_wrapper
+
 
 def with_async_session(*args):
     # this function can either be used as an async generator or decorator for transaction or nested transaction
     # for nested transaction, first parameter to the function should be True boolean value
-    
+
     session_factory = async_sessionmaker_context.get()
     if len(args) > 0:
         async_callable = None
@@ -54,12 +59,14 @@ def with_async_session(*args):
             nested_transaction_flag = args[0]
 
         if len(args) == 1:
+
             def session_wrap(async_callable):
-                return _session_wrap(session_factory, async_callable, nested=nested_transaction_flag)
+                return _session_wrap(
+                    session_factory, async_callable, nested=nested_transaction_flag
+                )
 
             return session_wrap
-        
-        return session_wrap(session_factory, async_callable, nested=nested_transaction_flag)
 
-
-
+        return session_wrap(
+            session_factory, async_callable, nested=nested_transaction_flag
+        )
--- src/sqla/extras/asyncio.py
+++ src/sqla/extras/asyncio.py
@@ -1,65 +1,88 @@
 import inspect
 import functools
+import logging
 from contextvars import ContextVar
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
-async_session_context: ContextVar[AsyncSession] = ContextVar("async_session_context", default=None)
-async_sessionmaker_context: ContextVar[async_sessionmaker] = ContextVar("async_sessionmaker_context", default=None)
+logger = logging.getLogger(__name__)
 
-def _session_wrap(session_factory: async_sessionmaker, async_callable, is_nested=False):
+async_session_context: ContextVar[AsyncSession] = ContextVar(
+    "async_session_context", default=None
+)
+async_sessionmaker_context: ContextVar[async_sessionmaker] = ContextVar(
+    "async_sessionmaker_context", default=None
+)
+
+
+async def _run_callable(
+    session: AsyncSession,
+    async_callable,
+    auto=True,
+    bind=False,
+    nested=None,
+    *args,
+    **kwargs
+):
+    if bind == True:
+        args = (session, *args)
+
+    result = None
+    try:
+        result = await async_callable(*args, **kwargs)
+    except Exception as e:
+        logger.error(e, exc_info=1)
+        raise e
+
+    return result
+
+
+def with_async_session(async_callable, auto=True, bind=False, nested=False):
+    # this function can either be used as an async generator or decorator for transaction or nested transaction
+    # for nested transaction, first parameter to the function should be True boolean value
+
+    session_factory = async_sessionmaker_context.get()
+
     @functools.wraps(async_callable)
     async def session_wrapper(*args, **kwargs):
         existing_session = async_session_context.get()
 
-        if is_nested == True and existing_session is None:
-            raise Exception("Parent session not started")
-        
-        if is_nested == True and existing_session:
-            async with existing_session.begin_nested() as nested:
-                try:
-                    result = await async_callable(*args, **kwargs)
-                    nested.commit()
-                    return result
-                except Exception as e:
-                    nested.rollback()
+        if nested == True and existing_session is None:
+            raise Exception("cannot run nested transaction without session initialized")
 
+        if nested == True and existing_session:
+            async with existing_session.begin_nested():
+                return await _run_callable(
+                    existing_session,
+                    async_callable,
+                    auto=auto,
+                    bind=bind,
+                    nested=nested,
+                    *args,
+                    **kwargs
+                )
 
-        if existing_session:
-            async with existing_session:
-                return await async_callable(*args, **kwargs)
-                
-        async with session_factory() as session:
-            async with session:
-                async_session_context.set(session)
-                return await async_callable(*args, **kwargs)
-    
+        if nested == False and existing_session:
+            async with existing_session.begin():
+                return await _run_callable(
+                    existing_session,
+                    async_callable,
+                    auto=auto,
+                    bind=bind,
+                    nested=None,
+                    *args,
+                    **kwargs
+                )
+
+        async with session_factory() as session:  # type: AsyncSession
+            async with session.begin():
+                return await _run_callable(
+                    session,
+                    async_callable,
+                    auto=auto,
+                    bind=bind,
+                    nested=None,
+                    *args,
+                    **kwargs
+                )
+
     return session_wrapper
-
-def with_async_session(*args):
-    # this function can either be used as an async generator or decorator for transaction or nested transaction
-    # for nested transaction, first parameter to the function should be True boolean value
-    
-    session_factory = async_sessionmaker_context.get()
-    if len(args) > 0:
-        async_callable = None
-        nested_transaction_flag = False
-
-        if inspect.iscoroutinefunction(args[0]):
-            async_callable = args[0]
-
-        if async_callable is not None:
-            return _session_wrap(session_factory, async_callable)
-
-        if isinstance(args[0], bool):
-            nested_transaction_flag = args[0]
-
-        if len(args) == 1:
-            def session_wrap(async_callable):
-                return _session_wrap(session_factory, async_callable, nested=nested_transaction_flag)
-
-            return session_wrap
-        
-        return session_wrap(session_factory, async_callable, nested=nested_transaction_flag)
-
-
-
